1b6964ee951087e3960f97ddf0562170e8a80540
Julia Okhrimenko <juliajam2497@gmail.com>
2022-02-02 14:16:02 +0200

    setted folder for img

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-undef */
 const path = require("path");
 const HtlmlPl = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   devServer: {
@@ -45,9 +45,17 @@ module.exports = {
     new HtlmlPl({
       template: "./src/index.html",
     }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: fileName("css"),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/img"),
+          to: path.resolve(__dirname, "./dist/img"),
+        },
+      ],
     }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+    new CleanWebpackPlugin(),
   ],
 };
