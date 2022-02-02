ee0eb441d4ced56c81b9779c7d5a64e3fbb3cf7e
Julia Okhrimenko <juliajam2497@gmail.com>
2022-02-02 14:30:57 +0200

    cleared unwanted

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-undef */
 const path = require("path");
 const HtlmlPl = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   devServer: {
@@ -22,6 +22,16 @@ module.exports = {
         use: ["file-loader"],
       },
       { test: /\.(js)$/, use: "babel-loader" },
+      {
+        test: /\.s[ac]ss/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
     ],
   },
 
@@ -35,5 +45,8 @@ module.exports = {
       template: "./src/index.html",
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
   ],
 };
